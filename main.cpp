b4f8fffe0d92e3e1d64f5d0d7bbd95d7ee824a8a
Larkin Koker <>
2021-04-20 17:49:43 +0000

    Everything

--- main.cpp
+++ main.cpp
@@ -1,22 +1,81 @@
-//Author: PUT YOUR NAME HERE
+//Author: Larkin Koker
 #include <iostream>
-#include "bitmap.h"
+#include "libraries/bitmap.h"
+#include <vector>
 using namespace std;
 
+bool imageValidator(bool, Bitmap);
+bool redDetect(Pixel, vector<vector<Pixel>>, bool);
+bool greenDetect(Pixel, vector<vector<Pixel>>, bool);
 
 int main()
 {
-Bitmap image;
-Pixel rgb;
-string file; 
-vector <vector<Pixel> > bmp;
-char decision;
+ string fileName;
+ Bitmap image;
+ vector <vector <Pixel> > bmp;
+ Pixel rgb;
+ bool validImage;
+ bool red;
+ bool green;
 
-cout<< "Enter file you want to open: ";
-cin>> file;
+ do{
+   cout<<"What file do you want to open (or 'no' to quit)?"<<endl;
+   getline(cin, fileName);
+   image.open(fileName);
+   validImage=imageValidator(validImage, image);
+   
+   
+   if(validImage==true){
+     bmp=image.toPixelMatrix();
+     
+     red=redDetect(rgb,bmp,red);
+     green=greenDetect(rgb,bmp,green);
+
+    if(red==true&&green==true){
+    cout<<"Red and green have been detected. Red/Green colorblindness is a common form of colorblindness meaning this image is dangerous to colorblind humans."<<endl;
+    }else{
+      cout<<"No red or green detected. Image is safe for Red/Green colorblind people."<<endl;
+    }
+
+   }else{
+     cout<<"What file do you want to open (or 'no' to quit)?"<<endl;
+   getline(cin, fileName);
+   }
+
+ }while (fileName !="no");
+
 
 
 
 
   return 0;
 }
+
+bool imageValidator(bool a, Bitmap image){
+  a = image.isImage();
+  return a;
+}
+bool redDetect(Pixel rgb, vector<vector<Pixel> > bmp,bool r){
+  for(int i=0; i<bmp.size();i++){
+  bmp[i][i] = rgb;
+  if(rgb.red>rgb.green&&rgb.red>rgb.blue){
+  r= true;
+  }else{
+  r=false;
+  }
+  }
+  return r;
+}
+
+bool greenDetect(Pixel rgb, vector<vector<Pixel>>bmp, bool g){
+ for(int i=0; i<bmp.size();i++){
+  bmp[i][i] = rgb;
+  if(rgb.red<rgb.green&&rgb.green>rgb.blue){
+  g= true;
+  }else{
+  g=false;
+  }
+ }
+  return g;
+}
+
